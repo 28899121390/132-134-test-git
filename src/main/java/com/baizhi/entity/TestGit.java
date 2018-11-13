281972b618e29d57ab875b952026a5a74d824294
htf <2889921390@qq.com>
2018-11-13 14:34:00 +0800

    woshilisi

--- src/main/java/com/baizhi/entity/TestGit.java
+++ src/main/java/com/baizhi/entity/TestGit.java
@@ -3,5 +3,6 @@ package com.baizhi.entity;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("i am zhangsan");
+        System.out.println("i am lisi");
     }
 }
